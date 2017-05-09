a783af1d51feca76b17bc527cc462684e28c8022
Administrator <18393761804@qq.com>
2017-05-09 10:07:07 +0800

    beautiful girl

--- Day0509LianXi2/app/src/main/java/com/bawei/day0509lianxi2/MainActivity.java
+++ Day0509LianXi2/app/src/main/java/com/bawei/day0509lianxi2/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-		//ÈÃ´úÂëÆğ³åÍ»°É
+		//GuoXueBing is a beautiful girl!
     }
 }
