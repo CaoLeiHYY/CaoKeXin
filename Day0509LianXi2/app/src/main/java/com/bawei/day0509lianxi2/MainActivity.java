9e655b53000a108cfea89ae722957038e49765fb
Administrator <18393761804@qq.com>
2017-05-09 09:53:42 +0800

    tijiao

---
+++ Day0509LianXi2/app/src/main/java/com/bawei/day0509lianxi2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bawei.day0509lianxi2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+		//Á·Ï°½â¾ö´úÂë³åÍ»
+    }
+}
